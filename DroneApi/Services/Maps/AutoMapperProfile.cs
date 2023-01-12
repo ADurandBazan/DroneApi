b1e3565b01e05fb70166afcbd98a918c11bcf288
ADurandBazan <adurandbazan@gmail.com>
2023-01-11 20:04:35 -0500

    using automaper to maps create profile to maps dto to entity and vice versa

---
+++ DroneApi/Services/Maps/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DroneApi.Entities;
+
+namespace DroneApi.Services.Maps
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // DroneDto -> Drone
+            CreateMap<DroneDto, Drone>();
+            // Drone -> DroneDto
+            CreateMap<Drone, DroneDto>();
+
+        }
+    }
+}
