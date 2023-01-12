e986c48d10f8ebbbf1469cc5c9dcb1eebc47cee8
ADurandBazan <adurandbazan@gmail.com>
2023-01-11 21:35:33 -0500

    implement drone controller

---
+++ DroneApi/Controllers/DroneController.cs
@@ -0,0 +1,91 @@
+﻿using DroneApi.Common.Filters;
+using DroneApi.Services.IService;
+using DroneApi.Services.Maps;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DroneApi.Controllers
+{
+    [ApiController]
+    [Route("api/drone")]
+    public class DroneController : ControllerBase
+    {
+        private readonly IDroneService _droneService;
+
+        public DroneController(IDroneService droneService)
+        {
+            _droneService = droneService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] DroneRequestFilter? request = null)
+        {
+            var drones = await _droneService.FindAllDronesAsync(request);
+            return Ok(new
+            {
+                success = true,
+                drones
+            });
+        }
+       
+        [HttpGet("getmedications/{id}")]
+        public async Task<IActionResult> GetMedicationsById(int id)
+        {
+            var medications = await _droneService.GetMedicationsByIdAsync(id);
+            return Ok(new
+            {
+                success = true,
+                medications
+            });
+        }
+        [HttpGet("batterycheck/{id}")]
+        public async Task<IActionResult> GetBatteryCapacityById(int id)
+        {
+            var battery = await _droneService.GetBatteryCapacityByIdAsync(id);
+            return Ok(new
+            {
+                success = true,
+                battery
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(DroneDto droneDto)
+        {
+            var drone = await _droneService.InsertDroneAsync(droneDto);
+            if (drone != null)
+                return Ok(new
+                {
+                    success = true,
+                    message = "Drone created",
+                    drone
+                });
+
+            return StatusCode(500);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(DroneDto droneDto)
+        {
+            var drone = await _droneService.UpdateDroneAsync(droneDto);
+            if (drone != null)
+                return Ok(new
+                {
+                    success = true,
+                    message = "Drone updated",
+                    drone
+                });
+
+            return NotFound(new { success = false, message = "Drone not found" });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _droneService.DeleteDroneAsync(id);
+
+            return Ok(new { success = true, message = "Drone deleted" });
+        }
+    }
+}
+
