e372e358cabedde33cd7229b2130db907ff60b8d
ADurandBazan <adurandbazan@gmail.com>
2023-01-12 00:47:47 -0500

    add data annotation key attribute to id property in entity

--- DroneApi/Entities/Entity.cs
+++ DroneApi/Entities/Entity.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DroneApi.Entities
 {
     public abstract class Entitity<TId>
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual TId Id
         {
