6723eea8e863da2bcba6b8b74730d9aa3902a188
ADurandBazan <adurandbazan@gmail.com>
2023-01-12 15:05:49 -0500

    solved problem to dronebatterylog controller add route to dronebatterylog controller

--- DroneApi/Controllers/DroneBatteryLogController.cs
+++ DroneApi/Controllers/DroneBatteryLogController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DroneApi.Controllers
 {
+    [ApiController]
+    [Route("api/dronebatterylogs")]
     public class DroneBatteryLogController : ControllerBase
     {
         private readonly IDroneBatteryLogService _droneBatteryLogService;
